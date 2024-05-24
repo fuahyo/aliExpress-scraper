49fc2f4ecaed212565d5caa7c0b9971f5be2a4f4
Galang Dwi Juniar <galang@Galang.local>
2024-05-24 22:44:15 +0700

    add listing

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -69,12 +69,14 @@ total_page = total_page_summary.scan(/\d+/).first.to_i
         url = "https://www.aliexpress.com/category/100003109/women-clothing.html?page=#{i}"
         pages << {
             # method: "GET",
-            # fetch_type: "browser",
+            fetch_type: "browser",
             # headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
             # vars: {
             #     category: "Women's clothing"
             # },
             url:url,
+
+            page_type: 'products',
             page_type: "listings",
             # display: {
             #     "width": 1920,
