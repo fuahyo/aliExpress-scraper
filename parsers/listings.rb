bc6bab3dec1b2454050a323c9f7afcd5e68ef42b
Galang Dwi Juniar <galang@Galang.local>
2024-05-27 18:13:57 +0700

    test

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,30 +1,69 @@
 nokogiri = Nokogiri.HTML(content)
-listings = nokogiri.css('.search-item-card-wrapper-gallery')
-# listings = nokogiri.css('#card-list .search-item-card-wrapper-gallery')
+products = nokogiri.css('.search-item-card-wrapper-gallery')
+products.each do |product|
+  a_element = product.at_css('.card--out-wrapper a')
+  if a_element
+    url = URI.join('https:', a_element['href']).to_s.split('?').first
+    if url =~ /\Ahttps?:\/\//i
+      pages << {
+          url: url,
+          page_type: 'products',
+          fetch_type: 'browser',
+          force_fetch: true,
+          vars: {
+            category: page['vars'],
+            url: url
+          }
+        }
+    end
+  end
+end
 
-listings.each do |listing|
-    product = {}
-    a_element = listing.at_css('.card--out-wrapper a').attr('href')
-    if a_element
-
-        product['url'] = URI.join('https:', a_element).to_s.split('?').first
-
-        product['_collection'] = "listings"
-
-        # save the product to the outputs.
-        outputs << product
-
-        # if product['url'] =~ /\Ahttps?:\/\//i
-            pages << {
-                url: product['url'],
-                page_type: 'products',
-                fetch_type: 'browser',
-                force_fetch: true,
-                vars: {
-                    # category: page['vars']['category'],
-                    url: product['url']
-                }
-            }
-        # end
+total_page_summary = nokogiri.css("li.comet-pagination-item.comet-pagination-item-60 > a").text
+total_page = total_page_summary.scan(/\d+/).first.to_i
+2.upto(total_page) do |i|
+    if i < 11
+        pageUrl = "https://www.aliexpress.com/category/100003109/women-clothing.html?page=#{i}"
+        
+        pages << {
+            fetch_type: "browser",
+            page_type: "listings",
+            url:pageUrl,        
+            # headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+            vars: {
+                category: "Women's clothing"
+            },
+        }
     end
 end
+
+# listings = nokogiri.css('.search-item-card-wrapper-gallery')
+# # listings = nokogiri.css('#card-list .search-item-card-wrapper-gallery')
+
+# listings.each do |listing|
+#     product = {}
+#     a_element = listing.at_css('.card--out-wrapper a').attr('href')
+#     if a_element
+
+#         product['url'] = URI.join('https:', a_element).to_s.split('?').first
+
+#         product['_collection'] = "listings"
+
+#         # save the product to the outputs.
+#         outputs << product
+
+#         # if product['url'] =~ /\Ahttps?:\/\//i
+#             pages << {
+#                 url: product['url'],
+#                 page_type: 'products',
+#                 fetch_type: 'browser',
+#                 force_fetch: true,
+#                 vars: {
+#                     # category: page['vars']['category'],
+#                     category: page['vars'],
+#                     url: product['url']
+#                 }
+#             }
+#         # end
+#     end
+# end
--- parsers/products.rb
+++ parsers/products.rb
@@ -3,66 +3,67 @@ html = Nokogiri.HTML(content)
 product = {}
 
 url = page['url']
+puts url
 
-product['url'] = url
+# product['url'] = url
 
-# product['category'] = page['vars']['category']
+# # product['category'] = page['vars']['category']
 
-product['title'] = html.css('h1.product-title-text').text.strip
+# product['title'] = html.css('h1.product-title-text').text.strip
 
-image_url = html.at_css('.images-view-item img')
+# image_url = html.at_css('.images-view-item img')
 
-product['image_url'] = image_url['src']
+# product['image_url'] = image_url['src']
 
-discount_elem = html.css('div.product-price-current')
-discount_elem = html.css('span.uniform-banner-box-price') if discount_elem.empty?
+# discount_elem = html.css('div.product-price-current')
+# discount_elem = html.css('span.uniform-banner-box-price') if discount_elem.empty?
 
-if discount_elem
-    pricestring = discount_elem.text
-    price = pricestring.scan(/(\d+\.\d+)/)
-    product['discount_low_price'] = price.first[0].to_f
-    product['discount_high_price'] = price.last[0].to_f
-end
+# if discount_elem
+#     pricestring = discount_elem.text
+#     price = pricestring.scan(/(\d+\.\d+)/)
+#     product['discount_low_price'] = price.first[0].to_f
+#     product['discount_high_price'] = price.last[0].to_f
+# end
 
-originalprice_elem = html.css('div.product-price-original')
-originalprice_elem = html.css('span.uniform-banner-box-discounts') if originalprice_elem.empty?
-originalprice_elem = html.css('div.product-price-current') if originalprice_elem.empty?
+# originalprice_elem = html.css('div.product-price-original')
+# originalprice_elem = html.css('span.uniform-banner-box-discounts') if originalprice_elem.empty?
+# originalprice_elem = html.css('div.product-price-current') if originalprice_elem.empty?
 
 
-if originalprice_elem
-    valueprice = originalprice_elem.css('div.product-price-del')
+# if originalprice_elem
+#     valueprice = originalprice_elem.css('div.product-price-del')
    
-    pricestring = valueprice.empty? || valueprice.nil? ? originalprice_elem.text : valueprice.text
+#     pricestring = valueprice.empty? || valueprice.nil? ? originalprice_elem.text : valueprice.text
 
-    price = pricestring.scan(/(\d+\.\d+)/)
-    product['original_low_price'] = price.first[0].to_f
-    product['original_high_price'] = price.last[0].to_f
-end
+#     price = pricestring.scan(/(\d+\.\d+)/)
+#     product['original_low_price'] = price.first[0].to_f
+#     product['original_high_price'] = price.last[0].to_f
+# end
 
-sku = url.split('/')
+# sku = url.split('/')
 
-product['sku'] =  sku.last.split('.').first
+# product['sku'] =  sku.last.split('.').first
 
-size_elem = html.css('div.product-sku div.sku-property[2] li.sku-property-item')
+# size_elem = html.css('div.product-sku div.sku-property[2] li.sku-property-item')
 
-if size_elem
-    size = size_elem.css('div.sku-property-text').collect{|s| s.text.strip}
-    product['size'] = size
-end
+# if size_elem
+#     size = size_elem.css('div.sku-property-text').collect{|s| s.text.strip}
+#     product['size'] = size
+# end
 
-rating = html.at_css('.overview-rating-average')&.text
-reviews_count = html.at_css('.product-reviewer-reviews')&.text
+# rating = html.at_css('.overview-rating-average')&.text
+# reviews_count = html.at_css('.product-reviewer-reviews')&.text
 
-product['rating'] = rating.to_f if rating
-product['reviews_count'] = reviews_count.to_f if reviews_count
+# product['rating'] = rating.to_f if rating
+# product['reviews_count'] = reviews_count.to_f if reviews_count
 
-product['ship_info'] = html.at_css('.product-shipping-price')&.text
+# product['ship_info'] = html.at_css('.product-shipping-price')&.text
 
-return_policy = html.css('.buyer-pretection-info[2] .buyer-pretection-context')
-product['return_policy'] = return_policy.text.strip if return_policy
+# return_policy = html.css('.buyer-pretection-info[2] .buyer-pretection-context')
+# product['return_policy'] = return_policy.text.strip if return_policy
 
-product['guarantee'] = html.at_css('.buyer-pretection-context')&.text
+# product['guarantee'] = html.at_css('.buyer-pretection-context')&.text
 
-product['_collection'] = "products"
+# product['_collection'] = "products"
 
 outputs << product
